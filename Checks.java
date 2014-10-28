0fb5636e2e495f591a29567eb5fc2dba54ad96a9
Slaff1994 <slaff1994@hotmail.com>
2014-10-28 15:24:46 -0400

    Version 1
    The Actual Original

--- Checks.java
+++ Checks.java
@@ -12,9 +12,13 @@ package Chess;
 public class Checks {
     int PlayerCount;
     int AICount;
+    int capture_count;
+    int movecount;
     boolean Stalemate;
     boolean Win;
     boolean AvailableMove;
+    boolean checkCapture;
+    
     
     public void checkWin(){
         if(AICount == 0 || PlayerCount == 0){
@@ -34,5 +38,39 @@ public class Checks {
         else{
             Stalemate = false;
         }
+    }
+    public boolean checkAvailableMove(){
+        if(movecount > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean checkCapture(){
+        if(capture_count > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void getCaptureCount(ChessPieces[][] inBoard, int player){
+        ChessPieces[][] board = inBoard;
+        movecount = 0;
+        capture_count = 0;
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                if(board[i][j].getPlayer() == player){
+                    movecount += board[i][j].MovesPossible(board, i, j);
+                    capture_count += board[i][j].CapturesPossible(board, i, j);
+                    
+                }
+            }
+        }
+        if(movecount > 0 || capture_count > 0){
+            AvailableMove = true;
+        }
+        
     }
 }
